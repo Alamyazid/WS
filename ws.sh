829d74be3cb02285b05adcca120fa2d24089c1f2
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-05 22:28:27 +0800

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -4,7 +4,7 @@
 
 # Getting Proxy Template
 wget https://raw.githubusercontent.com/Alamyazid/WS/main/usernew.sh
-rm -r /user/bin/usernew
+rm -r /usr/bin/usernew
 chmod +x usernew.sh
 wget -q -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Alamyazid/WS/main/ws.py
 chmod +x /usr/local/bin/edu-proxy
